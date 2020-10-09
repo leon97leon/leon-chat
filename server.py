eb9f720d40e1017a0479d4ff5207b380710976b3
leon97leon <48119463+leon97leon@users.noreply.github.com>
2020-10-09 15:23:39 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import SocketIO, emit, join_room, leave_room
 
-# Next two lines are for the issue: https://github.com/miguelgrinberg/python-engineio/issues/142
 from engineio.payload import Payload
 Payload.max_decode_packets = 200
 
